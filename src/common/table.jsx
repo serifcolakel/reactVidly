cd6954365b85a5c666676d2adacbaba3c35f6d0b
Serif Colakel <serifcolakel0@gmail.com>
2021-05-13 00:00:03 +0300

    first commit

---
+++ src/common/table.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import TableHeader from "../common/tableHeader";
+import TableBody from "../common/tableBody";
+
+const Table = (props) => {
+  const { columns, sortColumn, onSort, data } = props;
+  return (
+    <table class="table">
+      <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
+      <TableBody columns={columns} data={data} />
+    </table>
+  );
+};
+
+export default Table;
