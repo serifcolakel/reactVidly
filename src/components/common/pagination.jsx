775fe512a11bc64e10c4a283433c10d6ef1a66ed
Serif Colakel <serifcolakel0@gmail.com>
2021-06-05 13:27:49 +0300

    pagination error

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -15,7 +15,7 @@ const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
             key={page}
             className={page === currentPage ? "page-item active" : "page-item"}
           >
-            <a className="page-link" onClick={() => onPageChange(page)}>
+            <a className="page-link" href="" onClick={() => onPageChange(page)}>
               {page}
             </a>
           </li>
